2b8a24bf487cd9cfc7190f49fbd78849d410a0cd
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-07-28 12:42:19 +0200

    refactor: Update RegistrationForm component to use reactive forms
    The RegistrationForm component has been updated to use reactive forms for better form validation and handling. The component now imports the necessary modules from '@angular/forms' and '@angular/common'. The form fields have been defined using the FormBuilder and Validators classes, with added validation for name, surname, phone number, email, password, address, postal code, and city. A custom validator function, confirmPasswordValidator, has also been added to validate the password confirmation field. The onSubmit() method has been implemented to handle form submission.
    
    This commit refactors the RegistrationForm component to improve form functionality and validation.

--- src/app/pages/Login/RegistrationForm/RegistrationForm.ts
+++ src/app/pages/Login/RegistrationForm/RegistrationForm.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  ValidatorFn,
+  ValidationErrors,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: '[component="RegistrationForm"]',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './RegistrationForm.html',
 })
-export class RegistrationForm {
-  title = 'REGISTRATION';
+export class RegistrationForm implements OnInit {
+  registrationForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group(
+      {
+        nome: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+        cognome: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+        cellulare: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required]],
+        confermaPassword: ['', [Validators.required, confirmPasswordValidator]],
+        indirizzi: [
+          '',
+          [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')],
+        ],
+        cap: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(5),
+            Validators.pattern('^[0-9]+$'),
+          ],
+        ],
+        citta: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+      },
+      { validator: confirmPasswordValidator }
+    );
+  }
+
+  onSubmit(): void {
+    if (this.registrationForm.valid) {
+      console.log('Form Submitted!', this.registrationForm.value);
+    }
+  }
 }
+
+export const confirmPasswordValidator: ValidatorFn = (
+  formGroup: AbstractControl
+): ValidationErrors | null => {
+  const password = formGroup.get('password');
+  const confermaPassword = formGroup.get('confermaPassword');
+  if (!password || !confermaPassword) {
+    return null;
+  }
+  if (password.value == confermaPassword.value) return null;
+  else return { confermaPassword: true };
+};
