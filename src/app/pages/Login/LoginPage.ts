bcc6744e02270885b9b577222606f50f639c4e8e
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-08-15 22:17:37 +0200

    refactor: Update package.json and package-lock.json to add Bootstrap v5.3.3 dependency

--- src/app/pages/Login/LoginPage.ts
+++ src/app/pages/Login/LoginPage.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginForm } from './LoginForm/LoginForm';
 import { RegistrationForm } from './RegistrationForm/RegistrationForm';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'LoginPage',
@@ -10,4 +11,7 @@ import { RegistrationForm } from './RegistrationForm/RegistrationForm';
 })
 export class LoginPage {
   title = 'LOGIN';
+  constructor(private titleSvc: Title) {
+    this.titleSvc.setTitle('Accedi al tuo account | Take2Me');
+  }
 }
