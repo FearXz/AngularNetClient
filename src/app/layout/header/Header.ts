9562c8d2be178d5bc95235aa8c1e8f12e5167169
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-08-15 19:25:12 +0200

    refactor: Update Header.html and Header.ts to add login and contacts buttons

--- src/app/layout/header/Header.ts
+++ src/app/layout/header/Header.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'Header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './Header.html',
 })
 export class Header {
+  currentFlag = '../../../assets/images/utilities/it.svg';
   title = 'HEADER';
 }
